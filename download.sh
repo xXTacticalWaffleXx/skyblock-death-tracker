1fdfe235362ba3a0c0a6dc3a54f25953d19ee0a1
Luna Aphelion <luna-aphelion@outlook.com>
2022-08-08 19:57:12 +0100

    added a copyright disclaimer in the scripts

--- download.sh
+++ download.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+
+#author: luna aphelion
+#//contact: luna-aphelion@outlook.com
+#
+#Copyright(C) 2022 luna aphelion
+#
+#This program is free software : you can redistribute itand /or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program.If not, see < https://www.gnu.org/licenses/>.
+
 curl https://raw.githubusercontent.com/xXTacticalWaffleXx/skyblock-death-tracker/main/discord_webhook.h > discord_webhook.h
 curl https://raw.githubusercontent.com/xXTacticalWaffleXx/skyblock-death-tracker/main/discord_webhook.cc > discord_webhook.cc
 curl https://raw.githubusercontent.com/xXTacticalWaffleXx/skyblock-death-tracker/main/main.cpp > main.cpp
