eb8c7fa6c4fdea5cd370e8aa6a80a0da6024f748
Luna Aphelion <luna-aphelion@outlook.com>
2022-08-08 16:10:40 +0100

    added some cli utilites used to get the program

---
+++ compile.sh
@@ -0,0 +1,1 @@
+g++ -O2 main.cpp discord_webhook.cc -lcurl -ljsoncpp -lcurlpp
---
+++ download.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl https://raw.githubusercontent.com/xXTacticalWaffleXx/skyblock-death-tracker/main/discord_webhook.h > discord_webhook.h
+curl https://raw.githubusercontent.com/xXTacticalWaffleXx/skyblock-death-tracker/main/discord_webhook.cc > discord_webhook.cc
+curl https://raw.githubusercontent.com/xXTacticalWaffleXx/skyblock-death-tracker/main/main.cpp > main.cpp
